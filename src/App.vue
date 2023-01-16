ceb5b098ca03960f5a46c9a1d99acc2ba7a5f74c
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2023-01-16 14:32:25 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+  import { store } from './store';
+  import AppHeader from './components/AppHeader.vue';
+  import AppMain from './components/AppMain.vue';
+
+export default{
+  components:{
+    AppHeader,
+    AppMain,
+  },
+  data() {
+    return {
+      store,
+    }
+  },
+
+}
+
+</script>
+
+<template>
+  <AppHeader/>
+  <AppMain />
+</template>
+
+<style lang="scss">
+  @use './style/general.scss' as*;
+  @use './style/partial/variables' as*;
+  
+</style>
